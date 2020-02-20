ee77fd17ee624007f4017aed661c61644d27fb81
Eugenio Marletti <takhion@gmail.com>
2020-02-20 11:02:32 +0100

    Enable better testing output

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 allprojects {
     group = "com.jetbrains.handson.mpp"
+
+    tasks.withType(Test::class) {
+        /** disable skipping up-to-date tests tasks */
+        outputs.upToDateWhen { false }
+
+        /** enable logging of tests lifecycle/output */
+        testLogging {
+            events(*enumValues<TestLogEvent>())
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
 }
